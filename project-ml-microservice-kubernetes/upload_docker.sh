1bb81a998271932c1b9549f2921118d2a5d1db13
Josh <jballantine@kx.com>
2022-09-10 02:49:19 +0000

    Init changes

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,13 +5,11 @@
 
 # Step 1:
 # Create dockerpath
-dockerpath=jballantine1/finalproj
+# dockerpath=<your docker ID/path>
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-docker tag predictions $dockerpath
 
 # Step 3:
 # Push image to a docker repository
-docker push $dockerpath
